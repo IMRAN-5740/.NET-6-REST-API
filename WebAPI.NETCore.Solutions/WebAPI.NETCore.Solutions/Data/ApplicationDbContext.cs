7afbba372139fc2236e6c0a2ad79e754ee6ae3bb
imran <imranbsmrstucse@gmail.com>
2022-12-25 16:25:00 +0600

    Conection Build Up and Models Added

---
+++ WebAPI.NETCore.Solutions/WebAPI.NETCore.Solutions/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.NETCore.Solutions.Models;
+
+namespace WebAPI.NETCore.Solutions.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> _dbContext):base(_dbContext)
+        {
+
+
+        }
+
+      
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
---
+++ WebAPI.NETCore.Solutions/WebAPI.NETCore.Solutions/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.NETCore.Solutions.Models
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Please Provide Product Name")]
+        [Display(Name="Product Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Please Provide Product Code")]
+        [Display(Name = "Product Code")]
+        public string Code { get; set; }
+        [Required(ErrorMessage = "Please Provide Description")]
+        public string Description { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Now;
+
+    }
+}
