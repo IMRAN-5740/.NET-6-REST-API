f1075be064cf9cde30b87f1ffaeab8b68c37432c
imran <imranbsmrstucse@gmail.com>
2022-12-26 11:27:28 +0600

    CRUD Perform Using API

--- WebAPI.NETCore.Solutions/WebAPI.NETCore.Solutions/Controllers/ProductsController.cs
+++ WebAPI.NETCore.Solutions/WebAPI.NETCore.Solutions/Controllers/ProductsController.cs
@@ -8,21 +8,20 @@ using WebAPI.NETCore.Solutions.Models;
 
 namespace WebAPI.NETCore.Solutions.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ProductsController : ControllerBase
     {
 
-       
+
         IProductManager _productManager;
         public ProductsController(IProductManager productManager)
         {
- 
             _productManager = productManager;
         }
 
         [HttpGet]
-        public List<Product> GetAll()
+        public ActionResult<List<Product>> GetAll()
         {
             var products = _productManager.GetAll().ToList();
             return products;
@@ -30,39 +29,66 @@ namespace WebAPI.NETCore.Solutions.Controllers
 
 
         [HttpGet("id")]
-        public Product GetById(int id)
+        public ActionResult<Product> GetById(int id)
         {
-            return _productManager.GetById(id);
-        }
-
-        [HttpPost] 
-        public Product CreateProduct(Product product)
-        {
-            bool isSaved=_productManager.Add(product);
-            if(isSaved)
-            { 
-                return product;
-            }
-            return null;
-           
-        }
-
-        [HttpPut("EditProduct")]
-      
-        public Product EditProduct(Product product)
-        {
-            if(product == null)
+            var product = _productManager.GetById(id);
+            if(product==null)
             {
-                return null;
+                return NotFound();
             }
+
+            return Ok(product);
+        }
+
+        [HttpPost("product")]
+       // [Route("CreateProduct")]
+        public  ActionResult<Product>CreateProduct(Product product)
+        {
+            bool isSaved = _productManager.Add(product);
+            if (isSaved)
+            {
+                return Ok("Product Create Has been Successfully");
+            }
+            return BadRequest("Not Create");
+
+        }
+
+        [HttpPut("product")]
+     //  [Route("product")]
+        public ActionResult<Product> EditProduct(Product product)
+        {
+            
+            if (product == null)
+            {
+                return NotFound();
+            }
+
             bool isUpate = _productManager.Update(product);
-            if(isUpate)
+            if (isUpate)
             {
-                return product;
+                return Ok(product);
             }
-            return null;
+            return BadRequest("Product Not Updated");
         }
 
+        [HttpDelete("id")]
+       // [Route("DeleteProduct")]
+        public ActionResult<string> Delete( int id)
+        {
+
+            var products = _productManager.GetById(id);
+            if (products == null)
+            {
+                return BadRequest("Corresponding Product Not Found"); 
+            }
+            bool isDelete = _productManager.Delete(products);
+            if (isDelete)
+            {
+                return Ok("Product Deleted Successfully");
+            }
+            return BadRequest("Product Not Deleted");
+
+        }
 
 
     }
