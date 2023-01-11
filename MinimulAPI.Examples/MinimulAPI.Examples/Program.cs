e20f3ebd0d81e1284d76dfa7a466ddb24d862c81
imran <imranbsmrstucse@gmail.com>
2023-01-11 09:41:53 +0600

    HttpVerbs Added

--- MinimulAPI.Examples/MinimulAPI.Examples/Program.cs
+++ MinimulAPI.Examples/MinimulAPI.Examples/Program.cs
@@ -2,7 +2,24 @@ var builder=WebApplication.CreateBuilder(args);
 var app=builder.Build();
 app.MapGet("/",()=>
 {
-    return "Hello World";
+    return "Call Get Method";
 }
 );
+
+
+app.MapPost("/", () =>
+{
+    return Results.Ok("Call Post Method");
+});
+
+app.MapPut("/", () =>
+{
+    return Results.Ok("Call Put Method");
+});
+app.MapDelete("/", () =>
+{
+    return Results.Ok("Call Delete Method");
+});
+
+
 app.Run();
